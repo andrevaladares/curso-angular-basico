3ae30b085346617d42a7a496e07c01025450bb3a
valadares <andre.valadares@gmail.com>
2020-07-07 10:51:15 -0300

    0.6.0 implementado

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero.model';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Windstorm'},
+  {id: 2, name: 'Spiderman'},
+  {id: 3, name: 'Wonderwoman'},
+  {id: 4, name: 'Ironman'},
+  {id: 5, name: 'Hulk'},
+  {id: 6, name: 'Windstorm'},
+];
