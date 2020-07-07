f30d4327a4a92643e30d152687aa60cc1f60f432
valadares <andre.valadares@gmail.com>
2020-07-07 10:03:38 -0300

    0.4.0 implementado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero.model';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  heroe = 'Homem-Aranha';
+  hero: Hero = {id: 1, name: 'Windstorm'};
 
   constructor() { }
 
